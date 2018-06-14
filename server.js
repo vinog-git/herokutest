b32f86f13c85390b6b08536a355ee60451bfd139
Vinoth G <bmw@Anakin-2.local>
2018-06-14 17:16:11 +0530

    Get port from Heroku

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ let path = require('path')
 app.get('/', (req, res) => res.send('Hello'))
 // app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port' + process.env.PORT || 3000));
