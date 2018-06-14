208bc2af6a5b3b622416d640dd1015b0e6294ada
Vinoth G <bmw@Anakin-2.local>
2018-06-14 18:13:15 +0530

    Port update

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express()
 // app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')))
 // app.use('/static', express.static(path.join(__dirname, 'src')))
 app.use('/', express.static('src'));
-var process = process || {env:{PORT : 3000}};
-var port = process.env.PORT;
+// var process = process || {env:{PORT : 3000}};
+// var port = process.env.PORT;
 
-app.listen(port, () => console.log('Example app listening on port ' + port));
+app.listen(process.env.PORT, () => console.log('Example app listening on port ' + process.env.PORT));
