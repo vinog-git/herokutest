e3023bcfae7111b4b9a7471fd18225ff2bd02c85
Vinoth G <bmw@Anakin-2.local>
2018-06-14 18:09:10 +0530

    Server fix

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const app = express()
-let path = require('path')
+// let path = require('path')
 
 // app.get('/', (req, res) => res.send('Hello'))
 // app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')))
-
+// app.use('/static', express.static(path.join(__dirname, 'src')))
 app.use('/', express.static('src'));
-// var process = process || {env:{port : 3000}};
-// var port = process.env.port;
+var process = process || {env:{PORT : 3000}};
+var port = process.env.PORT;
 
-app.listen(process.env.port, () => console.log('Example app listening on port ' + process.env.port));
+app.listen(port, () => console.log('Example app listening on port ' + port));
