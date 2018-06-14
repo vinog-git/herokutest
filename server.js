415284a796f82cb2c1f2652e45faa0a4ea3dfd9d
Vinoth G <bmw@Anakin-2.local>
2018-06-14 17:31:15 +0530

    Testing heroku with serverJKs

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-const express = require('express')
-const app = express()
-let path = require('path')
+// const express = require('express')
+// const app = express()
+// let path = require('path')
+//
+// app.get('/', (req, res) => res.send('Hello'))
+// // app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')))
+//
+// app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port' + process.env.PORT || 3000));
 
-app.get('/', (req, res) => res.send('Hello'))
-// app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')))
-
-app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port' + process.env.PORT || 3000));
+setInterval(()=> console.log(new Date), 1000);
