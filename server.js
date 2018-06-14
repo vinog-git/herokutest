c5877c45bb31ead44b457c9b3e5ebfee740d4694
Vinoth G <bmw@Anakin-2.local>
2018-06-14 18:03:35 +0530

    Update environment for heroku

--- server.js
+++ server.js
@@ -6,8 +6,7 @@ let path = require('path')
 // app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')))
 
 app.use('/', express.static('src'));
+// var process = process || {env:{port : 3000}};
+// var port = process.env.port;
 
-var process = process || {env:{port : 3000}};
-var port = process.env.port;
-
-app.listen(port, () => console.log('Example app listening on port ' + port));
+app.listen(process.env.port, () => console.log('Example app listening on port ' + process.env.port));
